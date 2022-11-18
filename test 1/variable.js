175a5969d06de9cbb78696066e7cb1344f787e16
phu phan <phanvietphu92@gmail.com>
2022-11-18 14:06:01 +0700

    adding some file

--- test 1/variable.js
+++ test 1/variable.js
@@ -1,6 +1,7 @@
-var matrix = new Array(10).fill(new Array(10));
-for (var row = 0; row < matrix.length; row++) {
- for (var column = 0; column < matrix[row].length; column++) {
- console.log(matrix[row][column] + " ");
- }
-}
+var arr = new Array(10);
+    for (let i = 0; i<arr.length; i++){
+      arr[i]=  Math.floor((Math.random() * 100) + 1);
+    }
+    let newArr = arr.reverse();
+    console.log(arr);
+    console.log(newArr);
