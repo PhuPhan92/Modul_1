5c368466a91620e0c8945666bddc5a7e323f090b
phu phan <phanvietphu92@gmail.com>
2022-11-21 13:57:25 +0700

    adding some files

--- test 1/variable.js
+++ test 1/variable.js
@@ -1,11 +1,6 @@
 
-let arr = []
-let stringA = 'java'
-for (let i of stringA){
-      let j = 0
-      arr[j] = i
-      console.log(j)
-      console.log(i)
-      j++
-    }
-console.log(arr);
+let arr = new Array(+prompt('nhập vào size'))
+for(let i=0; i<arr.length; i++){
+  arr[i] = Math.floor(Math.random() * 10);
+}
+console.log(arr)
