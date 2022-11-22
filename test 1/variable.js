0ec99d31646e15714cf3f680c450f1de0f3190be
phu phan <phanvietphu92@gmail.com>
2022-11-22 08:55:26 +0700

    adding some files

--- test 1/variable.js
+++ test 1/variable.js
@@ -1,6 +1,26 @@
 
-let arr = new Array(+prompt('nhập vào size'))
-for(let i=0; i<arr.length; i++){
-  arr[i] = Math.floor(Math.random() * 10);
+let arr = [25,-5,-13,13]
+let arrnew = [];
+maxMinAvg(arr)
+function maxMinAvg(arr) {
+  let max = arr[0]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[0]) {
+      max = arr[i]
+    };
+  }
+  let min = arr[0];
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] < arr[0]) {
+      min = arr[j]
+    };
+  }
+  let sum = 0
+  for (let k = 0; k < arr.length; k++) {
+    sum += arr[k]
+  }
+  let avg = sum / arr.length;
+  arrnew.push(max,min,avg);
+  return arrnew;
 }
-console.log(arr)
+console.log(arrnew)
